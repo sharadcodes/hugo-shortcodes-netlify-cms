cee89eb70618dad9603ec4b0988822f13f0dd7ad
sharadcodes <sharadcodes@github.com>
2020-06-04 16:05:31 +0000

    Added Changes & Merged File

--- dist/hugo_shortcodes_netlify_cms.js
+++ dist/hugo_shortcodes_netlify_cms.js
@@ -125,14 +125,14 @@ CMS.registerEditorComponent({
         label: "Youtube Video ID",
         widget: "string"
     }],
-    pattern: /^youtube (\S+)$/,
+    pattern: /^{{< youtube ([a-zA-Z0-9]+) >}}/,
     fromBlock: function(match) {
         return {
             id: match[1],
         };
     },
     toBlock: function(obj) {
-        return "{{< youtube " + obj.id + ">}}";
+        return `{{< youtube ${obj.id} >}}`;
     },
     toPreview: function(obj) {
         return `<img src="http://img.youtube.com/vi/${obj.id}/maxresdefault.jpg" alt="Youtube Video"/>`;
