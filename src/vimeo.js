d2bf0b9a5efc03b8301525f3f93aa602c31da105
SHARAD RAJ <36638057+sharadcodes@users.noreply.github.com>
2020-06-04 17:41:14 +0530

    Create vimeo.js

---
+++ src/vimeo.js
@@ -0,0 +1,21 @@
+CMS.registerEditorComponent({
+    id: "vimeo",
+    label: "Vimeo",
+    fields: [{
+        name: "shortcode",
+        label: "Vimeo shortcode",
+        widget: "string"
+    }],
+    pattern: /^vimeo (\S+)$/,
+    fromBlock: function(match) {
+        return {
+            shortcode: match[1]
+        };
+    },
+    toBlock: function(obj) {
+        return `{{< vimeo ${obj.shortcode} >}}`;
+    },
+    toPreview: function(obj) {
+        return `{{< vimeo ${obj.shortcode} >}}`;
+    },
+});
