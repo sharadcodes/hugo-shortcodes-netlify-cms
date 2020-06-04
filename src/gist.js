bcd9d0b1fb925f4751f91115d39a24689aacf56d
SHARAD RAJ <36638057+sharadcodes@users.noreply.github.com>
2020-06-04 17:39:35 +0530

    Create gist.js

---
+++ src/gist.js
@@ -0,0 +1,28 @@
+CMS.registerEditorComponent({
+    id: "gist",
+    label: "Gist",
+    fields: [{
+            name: "username",
+            label: "Gist Username",
+            widget: "string"
+        },
+        {
+            name: "gid",
+            label: "Gist ID",
+            widget: "string"
+        },
+    ],
+    pattern: /^gist (\S+)$/,
+    fromBlock: function(match) {
+        return {
+            username: match[1],
+            gid: match[2],
+        };
+    },
+    toBlock: function(obj) {
+        return `{{< gist ${obj.username} ${obj.gid} >}}`;
+    },
+    toPreview: function(obj) {
+        return `{{< gist ${obj.username} ${obj.gid} >}}`;
+    },
+});
