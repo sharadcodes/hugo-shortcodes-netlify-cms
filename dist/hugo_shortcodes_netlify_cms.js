d16bb10424fbd6fea2032d7f93e099071e716ea6
sharadcodes <sharadcodes@github.com>
2020-06-05 07:02:27 +0000

    Added Changes & Merged File

--- dist/hugo_shortcodes_netlify_cms.js
+++ dist/hugo_shortcodes_netlify_cms.js
@@ -31,7 +31,7 @@ CMS.registerEditorComponent({
     label: "Gist",
     fields: [{
             name: "username",
-            label: "Gist Username",
+            label: "Github Username",
             widget: "string"
         },
         {
@@ -40,7 +40,7 @@ CMS.registerEditorComponent({
             widget: "string"
         },
     ],
-    pattern: /{{< gist ([a-zA-Z0-9-_]+) ([a-zA-Z0-9-_]+) >}}/,
+    pattern: /{{< gist ([a-zA-Z0-9]+) ([a-zA-Z0-9]+) >}}/,
     fromBlock: function(match) {
         return {
             username: match[1],
