e1865e2f57269f3dab57d13f2b8004b825609eb9
Brian Hurst <hursey013@protonmail.com>
2020-08-12 16:29:37 -0400

    Add hidecaption attribute

--- src/instagram.js
+++ src/instagram.js
@@ -1,21 +1,33 @@
 CMS.registerEditorComponent({
     id: "instagram",
     label: "Instagram",
-    fields: [{
-        name: "pid",
-        label: "Post id",
-        widget: "string"
-    }],
-    pattern: /{{< instagram ([a-zA-Z0-9]+) >}}/,
+    fields: [
+      {
+          name: "pid",
+          label: "Post id",
+          widget: "string"
+      },
+      {
+        name: "hidecaption",
+        label: "Hidden caption",
+        widget: "boolean"
+      }
+    ],
+    pattern: /{{< instagram ([a-zA-Z0-9]+) (hidecaption)?>}}/,
     fromBlock: function(match) {
         return {
-            pid: match[1]
+            pid: match[1],
+            hidecaption: match[2]
         };
     },
     toBlock: function(obj) {
-        return `{{< instagram ${obj.pid} >}}`;
+        return `{{< instagram ${obj.pid} ${
+          obj.hidecaption ? "hidecaption" : ""
+        }>}}`;
     },
     toPreview: function(obj) {
-        return `{{< instagram ${obj.pid} >}}`;
+        return `{{< instagram ${obj.pid} ${
+          obj.hidecaption ? "hidecaption" : ""
+        }>}}`;
     },
 });
