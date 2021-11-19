baf0eab0ab2ac41bf73b7c7193a07160820b476a
sharadcodes <sharadcodes@github.com>
2021-11-19 05:14:22 +0000

    Added Changes & Merged File

--- dist/hugo_shortcodes_netlify_cms.js
+++ dist/hugo_shortcodes_netlify_cms.js
@@ -57,22 +57,34 @@ CMS.registerEditorComponent({
 CMS.registerEditorComponent({
     id: "instagram",
     label: "Instagram",
-    fields: [{
-        name: "pid",
-        label: "Post id",
-        widget: "string"
-    }],
-    pattern: /{{< instagram ([a-zA-Z0-9]+) >}}/,
+    fields: [
+      {
+          name: "pid",
+          label: "Post id",
+          widget: "string"
+      },
+      {
+        name: "hidecaption",
+        label: "Hide caption",
+        widget: "boolean"
+      }
+    ],
+    pattern: /{{< instagram (?<pid>[a-zA-Z0-9]+)\s{0,}(?<hidecaption_flag>hidecaption)?\s+>}}/,
     fromBlock: function(match) {
         return {
-            pid: match[1]
+            pid: match[1],
+            hidecaption: match[2]
         };
     },
     toBlock: function(obj) {
-        return `{{< instagram ${obj.pid} >}}`;
+        return `{{< instagram ${obj.pid} ${
+          obj.hidecaption ? "hidecaption" : ""
+        }>}}`;
     },
     toPreview: function(obj) {
-        return `{{< instagram ${obj.pid} >}}`;
+        return `{{< instagram ${obj.pid} ${
+          obj.hidecaption ? "hidecaption" : ""
+        }>}}`;
     },
 });
 CMS.registerEditorComponent({
