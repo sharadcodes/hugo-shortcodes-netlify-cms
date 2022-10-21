8160aea5bde246a8ebc5a5553a240ae522479e1c
Thomas Clavier <tom@tcweb.org>
2022-10-21 19:12:09 +0200

    improve attributes management and preview
    Parse and manage 3 attributes : src, alt and title
    Use image widget to build img src for preview

--- src/figure.js
+++ src/figure.js
@@ -1,28 +1,41 @@
 CMS.registerEditorComponent({
     id: "figure",
-    label: "Figure",
+    label: "Illustration",
     fields: [{
-            name: "title",
-            label: "Figure Title",
-            widget: "string"
-        },
-        {
-            name: "src",
-            label: "Figure SRC",
-            widget: "string"
-        },
-    ],
-    pattern: /{{< figure src="([a-zA-Z0-9-_ ]+)" title="([a-zA-Z0-9-_ ]+)" >}}/,
-    fromBlock: function(match) {
-        return {
-            title: match[1],
-            src: match[2],
-        };
+        name: "title",
+        label: "Titre",
+        widget: "string",
+    }, {
+        name: "alt",
+        label: "Description",
+        widget: "string"
+    }, {
+        name: "src",
+        label: "Image",
+        widget: "image"
+    }],
+    pattern: /{{<\s*figure(.*)>}}/,
+    fromBlock: function (input) {
+        let output = {alt: "", src: "", title: ""}
+        let options = input[1].match(/\w+\s*=\s*"[^"]*"/g);
+        if (options) {
+            options.forEach((i) => {
+                const keyValue = i.split("=");
+                output = {...output, [keyValue[0]]: keyValue[1].replace(/"/g, '')}
+            });
+        }
+        return output;
     },
-    toBlock: function(obj) {
-        return `{{< figure src="${obj.src}" title="${obj.title}" >}}`;
+    toBlock: function (obj) {
+        let options = "";
+        options += obj.src ? ` src="${obj.src}"` : '';
+        options += obj.alt ? ` alt="${obj.alt}"` : '';
+        options += obj.title ? ` title="${obj.title}"` : '';
+        return `{{< figure ${options}>}}`;
     },
-    toPreview: function(obj) {
-        return `<figure><img src=${obj.src} alt=${obj.title}><figcaption>${obj.title}</figcaption></figure>`;
+    toPreview: ({title, alt, src}, getAsset, fields) => {
+        const imageField = fields?.find(f => f.get('widget') === 'image');
+        const imgSrc = getAsset(src, imageField);
+        return `<figure><img src="${imgSrc}" alt="${alt}"><figcaption><h4>${title}</h4></figcaption></figure>`;
     },
 });
